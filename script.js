aa89795fa84116948f562afa81723878aad8096e
Suraj kumar <sksurajkumarkumar7@gmail.com>
2026-07-29 14:33:18 +0530

    feat: Add student dashboard and fix login view switching bugs.

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 let loginform = document.getElementById("login-form");
 let emailinput = document.getElementById("user-email");
 let passwordinput = document.getElementById("user-password");
+const authsection = document.querySelector("#auth-section"); 
+const studentdashboard = document.getElementById("student-dashboard");
+const facultydashboard = document.getElementById("faculty-dashboard");
+const admindashboard = document.getElementById("admin-dashboard");
+
+const logoutbtn = document.querySelector(".btn-logout");
 
 loginform.addEventListener("submit",(e)=>{
     e.preventDefault();
     const email =emailinput.value;
     const password= passwordinput.value;
-    console.log('email:',email);
-    console.log('password:',password);
-
-    if(email==="admin@srgc.edu" && password==="123456"){
-        alert("Login Successful! Welcome to SRGC ERP.");
-    }else{
-        alert("Invalid Email or password! Try Again.");
-    }
+   
+    
+  if(email.includes("student") && password==="123456"){
+    authsection.classList.add("hidden");
+    studentdashboard.classList.remove("hidden");
+  }else if(email.includes("admin") && password==="123456"){
+    authsection.classList.add("hidden");
+    admindashboard.classList.remove("hidden");
+  }else if(email.includes("faculty") && password==="123456"){
+    authsection.classList.add("hidden");
+    facultydashboard.classList.remove("hidden");
+  }else{
+    alert("Invalid email or password! Try again");
+  }
 });
+if(logoutbtn){
+    logoutbtn.addEventListener("click",()=>{
+        studentdashboard.classList.add("hidden");
+        facultydashboard.classList.add("hidden");
+        admindashboard.classList.add("hidden");
+
+        authsection.classList.remove("hidden");
+    })
+}
